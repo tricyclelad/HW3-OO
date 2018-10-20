b458c5e5fc59b8b5088ba88012a62370781351a5
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-10-20 12:24:00 -0600

    Added test method for shape TEst
    Stable

--- Minimal (c-sharp)/ShapesTests/ShapeTest.cs
+++ Minimal (c-sharp)/ShapesTests/ShapeTest.cs
@@ -14,7 +14,19 @@ namespace ShapesTests
             Assert.AreEqual(1,p1.myPoints[0].X); 
             Assert.AreEqual(2, p1.myPoints[0].Y);
             Assert.AreEqual(p1.GetData(), "Name: " + "Point1" + "\tcolor: " + "Red");
-
         }
+        [TestMethod]
+        public void TestValidConstruction2()
+        {
+            Shape p1 = new Point("Point1", "Red", 1, 2);
+            Shape p2 = new Point("Point2", "Blue", 3, 4);
+            Shape L1 = new Line("Line1", "Black", (Point)p1, (Point)p2);
+            Assert.AreEqual(1,L1.myPoints[0].X); 
+            Assert.AreEqual(2, L1.myPoints[0].Y);
+            Assert.AreEqual(3, L1.myPoints[1].X);
+            Assert.AreEqual(4, L1.myPoints[1].Y);
+            Assert.AreEqual(L1.GetData(), "Name: " + "Line1" + "\tcolor: " + "Black");
+        }
+
     }
 }
