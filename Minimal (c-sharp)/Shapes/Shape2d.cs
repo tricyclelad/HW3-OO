93a32da41b70c525a87c55218998cb8b27b8c284
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-10-20 11:18:33 -0600

    Adding composite pattern to shapes

---
+++ Minimal (c-sharp)/Shapes/Shape2d.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public abstract class Shape2d
+    {
+        protected string name;
+
+        protected string color;
+        //protected double salary;
+
+        public Shape2d(string name, string color)
+
+        {
+            this.name = name;
+            this.color = color;
+        }
+
+        public abstract void Add(Shape2d _shape2d);
+
+        public abstract void Remove(Shape2d _shape2d);
+
+        public abstract string GetData();
+    }
+}
