1366eb3fe71356ea483289498e504f73b5b275f7
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-10-20 11:23:22 -0600

    Changed the rectangle to have official constructor

--- Minimal (c-sharp)/Shapes/Rectangle.cs
+++ Minimal (c-sharp)/Shapes/Rectangle.cs
@@ -15,12 +15,12 @@ namespace Shapes
         public Line Width { get; private set; }
         public Line Height { get; private set; }
 
-        public Rectangle(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
+        public Rectangle(string _name, string _color, double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4): base(_name, _color)
         {
-            var Vertex1 = new Point(x1,y1);
-            var Vertex2 = new Point(x2,y2);
-            var Vertex3 = new Point(x3,y3);
-            var Vertex4 = new Point(x4,y4);
+            var Vertex1 = new Point( _name + "'s Vertex1", _color, x1,y1);
+            var Vertex2 = new Point( _name + "'s Vertex2", _color, x2,y2);
+            var Vertex3 = new Point( _name + "'s Vertex3", _color, x3,y3);
+            var Vertex4 = new Point( _name + "'s Vertex4", _color, x4,y4);
             myPoints = new List<Point>();
             myPoints.Add(Vertex1);
             myPoints.Add(Vertex2);
@@ -31,8 +31,8 @@ namespace Shapes
             {
                 throw new ShapeException("Two or more Rectangle Vertices are equal");
             }
-            Width = new Line(Vertex1, Vertex2);
-            Height = new Line(Vertex1, Vertex4);
+            Width = new Line( _name + "'s Width", _color, Vertex1, Vertex2);
+            Height = new Line( _name + "'s Height", _color, Vertex1, Vertex4);
 
             if(!AreSidesSameLength())
             {
@@ -44,7 +44,7 @@ namespace Shapes
                 throw new ShapeException("Atleast one vertex is not squared with the other vertices");
             }
         }
-        public Rectangle(Point _Vertex1, Point _Vertex2, Point _Vertex3, Point _Vertex4)
+        public Rectangle(string _name, string _color, Point _Vertex1, Point _Vertex2, Point _Vertex3, Point _Vertex4) : base(_name, _color)
         {
             var Vertex1 = _Vertex1;
             var Vertex2 = _Vertex2;
@@ -60,8 +60,8 @@ namespace Shapes
             {
                 throw new ShapeException("Two or more Rectangle Vertices are equal");
             }
-            Width = new Line(Vertex1, Vertex2);
-            Height = new Line(Vertex1, Vertex4);
+            Width = new Line( _name + "'s Width", _color, Vertex1, Vertex2);
+            Height = new Line( _name + "'s Height", _color, Vertex1, Vertex4);
 
             if(!AreSidesSameLength())
             {
@@ -125,18 +125,18 @@ namespace Shapes
         }
         public bool AreSidesSameLength()
         {
-            Line Width1 = new Line(myPoints[0], myPoints[1]);
-            Line Width2 = new Line(myPoints[2], myPoints[3]);
-            Line Height1 = new Line(myPoints[0], myPoints[3]);
-            Line Height2 = new Line(myPoints[1], myPoints[2]);
+            Line Width1 = new Line("null","null",myPoints[0], myPoints[1]);
+            Line Width2 = new Line("null","null",myPoints[2], myPoints[3]);
+            Line Height1 = new Line("null","null",myPoints[0], myPoints[3]);
+            Line Height2 = new Line("null","null",myPoints[1], myPoints[2]);
             if (Validator.AreLineLengthsEqual(Width1, Width2) && Validator.AreLineLengthsEqual(Height1, Height2))
                 return true;
             return false;
         }
         public bool AreDiagonalsSameLength()
         {
-            Line Diagonal1 = new Line(myPoints[0], myPoints[2]);
-            Line Diagonal2 = new Line(myPoints[1], myPoints[3]);
+            Line Diagonal1 = new Line("null","null",myPoints[0], myPoints[2]);
+            Line Diagonal2 = new Line("null","null",myPoints[1], myPoints[3]);
             if (Validator.AreLineLengthsEqual(Diagonal1, Diagonal2))
                 return true;
             return false;
