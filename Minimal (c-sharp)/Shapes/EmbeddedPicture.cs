ba781000f91159b593d8a2061c5f8018a09aaf26
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-10-20 16:21:22 -0600

    Embedded PIcture supports move and compute area functions

---
+++ Minimal (c-sharp)/Shapes/EmbeddedPicture.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace Shapes
+{
+    public class EmbeddedPicture : Shape2d
+    {
+        Bitmap storedImage;
+        Double Width, Height;
+        List<Point> myEmbeddedPicturePoints;
+        
+        public EmbeddedPicture(string _name, string _color, Bitmap _storedImage):base(_name, _color)
+        {
+            storedImage = _storedImage; 
+        }
+        public override void Add(Shape2d _shape2d)
+        {
+            //Not used
+        }
+
+        public override double ComputeArea()
+        {
+            return Width * Height;
+        }
+
+        public override string GetData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Shape2d> GetMyShapes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Move(double deltaX, double deltaY)
+        {
+            foreach (var point in myEmbeddedPicturePoints)
+            {
+                point.Move(deltaX,deltaY);
+            }
+        }
+
+        public override void Remove(Shape2d _shape2d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
