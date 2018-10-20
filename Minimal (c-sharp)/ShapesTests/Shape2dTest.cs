15b38ebd32d1556e2b4463e45512885d5826dff8
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-10-20 13:45:05 -0600

    Finished shape 2d unit test
    May need to move the list of pionts from shape to 2dshape

--- Minimal (c-sharp)/ShapesTests/Shape2dTest.cs
+++ Minimal (c-sharp)/ShapesTests/Shape2dTest.cs
@@ -15,29 +15,28 @@ namespace ShapesTests
             Assert.AreEqual("Point1", shape2d.name);
             Assert.AreEqual("Red", shape2d.color);
         }
-        //[TestMethod]
-        //public void TestValidConstruction2()
-        //{
-        //    Shape p1 = new Point("Point1", "Red", 1, 2);
-        //    Shape p2 = new Point("Point2", "Blue", 3, 4);
-        //    Shape L1 = new Line("Line1", "Black", (Point)p1, (Point)p2);
-        //    Assert.AreEqual(1, L1.myPoints[0].X);
-        //    Assert.AreEqual(2, L1.myPoints[0].Y);
-        //    Assert.AreEqual(3, L1.myPoints[1].X);
-        //    Assert.AreEqual(4, L1.myPoints[1].Y);
-        //    Assert.AreEqual(L1.GetData(), "Name: " + "Line1" + "\tcolor: " + "Black");
-        //}
+        [TestMethod]
+        public void TestValidConstruction2()
+        {
+            Shape p1 = new Point("Point1", "Red", 1, 2);
+            Shape p2 = new Point("Point2", "Blue", 3, 4);
+            Shape L1 = new Line("Line1", "Black", (Point)p1, (Point)p2);
+            Shape2d Line2d = L1; 
+            Assert.AreEqual("Line1", Line2d.name);
+            Assert.AreEqual("Black", Line2d.color);
+        }
 
-        //[TestMethod]
-        //public void TestVoidFunctions()
-        //{
-        //    Shape myRectangle = new Rectangle("Rectangle1", "Fusion Blue", 0, 0, 0, 1, 1, 1, 1, 0);
-        //    Shape p1 = new Point("Point1", "Red", 1, 2);
-        //    Shape p2 = new Point("Point2", "Blue", 3, 4);
-        //    myRectangle.Add(p1);
-        //    myRectangle.Add(p2);
-        //    myRectangle.Remove(p1);
-        //    myRectangle.Remove(p2);
-        //}
+        [TestMethod]
+        public void TestVoidFunctions()
+        {
+            Shape myRectangle = new Rectangle("Rectangle1", "Fusion Blue", 0, 0, 0, 1, 1, 1, 1, 0);
+            Shape p1 = new Point("Point1", "Red", 1, 2);
+            Shape p2 = new Point("Point2", "Blue", 3, 4);
+            Shape2d rect2d = myRectangle;
+            rect2d.Add(p1);
+            rect2d.Add(p2);
+            rect2d.Remove(p1);
+            rect2d.Remove(p2);
+        }
     }
 }
