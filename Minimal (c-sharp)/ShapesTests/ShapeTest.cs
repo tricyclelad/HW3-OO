046bb0a03d9b2b8e2dd34c639896cbda5e462014
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-10-20 13:29:51 -0600

    Implemented unit test for shapeTest with void functions tested
    Stable

--- Minimal (c-sharp)/ShapesTests/ShapeTest.cs
+++ Minimal (c-sharp)/ShapesTests/ShapeTest.cs
@@ -11,7 +11,7 @@ namespace ShapesTests
         public void TestValidConstruction()
         {
             Shape p1 = new Point("Point1", "Red", 1, 2);
-            Assert.AreEqual(1,p1.myPoints[0].X); 
+            Assert.AreEqual(1, p1.myPoints[0].X);
             Assert.AreEqual(2, p1.myPoints[0].Y);
             Assert.AreEqual(p1.GetData(), "Name: " + "Point1" + "\tcolor: " + "Red");
         }
@@ -21,12 +21,23 @@ namespace ShapesTests
             Shape p1 = new Point("Point1", "Red", 1, 2);
             Shape p2 = new Point("Point2", "Blue", 3, 4);
             Shape L1 = new Line("Line1", "Black", (Point)p1, (Point)p2);
-            Assert.AreEqual(1,L1.myPoints[0].X); 
+            Assert.AreEqual(1, L1.myPoints[0].X);
             Assert.AreEqual(2, L1.myPoints[0].Y);
             Assert.AreEqual(3, L1.myPoints[1].X);
             Assert.AreEqual(4, L1.myPoints[1].Y);
             Assert.AreEqual(L1.GetData(), "Name: " + "Line1" + "\tcolor: " + "Black");
         }
 
+        [TestMethod]
+        public void TestVoidFunctions()
+        {
+            Shape myRectangle = new Rectangle("Rectangle1","Fusion Blue",0, 0, 0, 1, 1, 1, 1, 0);
+            Shape p1 = new Point("Point1", "Red", 1, 2);
+            Shape p2 = new Point("Point2", "Blue", 3, 4);
+            myRectangle.Add(p1);
+            myRectangle.Add(p2);
+            myRectangle.Remove(p1);
+            myRectangle.Remove(p2);
+        }
     }
 }
