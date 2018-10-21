66fb581d4f9adcc8bb3a9c78cbf3d6758e9b35e5
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-10-20 19:34:53 -0600

    The script class has a translator and load method
    Stable

---
+++ Minimal (c-sharp)/Shapes/Script.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Script
+    {
+        //        private static readonly DataContractJsonSerializer JsonSerializer = new DataContractJsonSerializer(typeof(List<TreeExtrinsicState>));
+
+        //    private readonly List<Tree> _trees = new List<Tree>();
+        //    private readonly object _myLock = new object();
+
+        //    public TreeFactory Factory { get; set; }
+        //    public bool IsDirty { get; private set; } = true;
+
+        //    public void Clear()
+        //    {
+        //        lock (_myLock)
+        //        {
+        //            _trees.Clear();
+        //            IsDirty = true;
+        //        }
+        //    }
+        public string[] translator(StreamReader _input)
+        {
+            string line = _input.ReadLine();
+            string[] translation = line.Split(null);
+            return translation;
+
+        }
+
+        public void Load(Stream input)
+        {
+            var reader = new StreamReader(input);
+            string[] parsedString = translator(reader);
+
+        }
+
+        //    public void Save(string filename)
+        //    {
+        //        var writer = new StreamWriter(filename);
+        //        var extrinsicStates = new List<TreeExtrinsicState>();
+        //        lock (_myLock)
+        //        {
+        //            foreach (var tree in _trees)
+        //            {
+        //                var t = tree as TreeWithAllState;
+        //                if (t != null)
+        //                    extrinsicStates.Add(t.ExtrinsicStatic);
+        //            }
+        //        }
+        //        JsonSerializer.WriteObject(writer.BaseStream, extrinsicStates);
+        //        writer.Close();
+        //    }
+
+        //    public void Add(Tree tree)
+        //    {
+        //        if (tree == null) return;
+        //        lock (_myLock)
+        //        {
+        //            _trees.Add(tree);
+        //            IsDirty = true;
+        //        }
+        //    }
+
+        //    public void DeleteAllSelected()
+        //    {
+        //        lock (_myLock)
+        //        {
+        //            _trees.RemoveAll(t => t.IsSelected);
+        //            IsDirty = true;
+        //        }
+        //    }
+
+        //    public void ToggleSelectionAtPosition(Point location)
+        //    {
+        //        var tree = FindTreeAtPosition(location);
+
+        //        if (tree != null)
+        //            tree.IsSelected = !tree.IsSelected;
+
+        //        IsDirty = true;
+        //    }
+
+        //    public Tree FindTreeAtPosition(Point location)
+        //    {
+        //        Tree result;
+        //        lock (_myLock)
+        //        {
+        //            result = _trees.FindLast(t => location.X >= t.Location.X &&
+        //                                          location.X < t.Location.X + t.Size.Width &&
+        //                                          location.Y >= t.Location.Y &&
+        //                                          location.Y < t.Location.Y + t.Size.Height);
+        //        }
+        //        return result;
+        //    }
+
+        //    public void DeselectAll()
+        //    {
+        //        lock (_myLock)
+        //        {
+        //            foreach (var t in _trees)
+        //                t.IsSelected = false;
+        //            IsDirty = true;
+        //        }
+        //    }
+
+        //    public bool Draw(Graphics graphics, bool redrawEvenIfNotDirty = false)
+        //    {
+        //        lock (_myLock)
+        //        {
+        //            if (!IsDirty && !redrawEvenIfNotDirty) return false;
+        //            graphics.Clear(Color.White);
+        //            foreach (var t in _trees)
+        //                t.Draw(graphics);
+        //            IsDirty = false;
+        //        }
+        //        return true;
+        //    }
+        //}
+    }
---
+++ Minimal (c-sharp)/Shapes/Shape2dFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Shape2dFactory
+    {
+        //CardFactory factory = null;
+        //Console.Write("Enter the card type you would like to visit: ");  
+        //    string car = Console.ReadLine();  
+  
+        //    switch (car.ToLower())  
+        //    {  
+        //        case "moneyback":  
+        //            factory = new MoneyBackFactory(50000, 0);  
+        //            break;  
+        //        case "titanium":  
+        //            factory = new TitaniumFactory(100000, 500);  
+        //            break;  
+        //        case "platinum":  
+        //            factory = new PlatinumFactory(500000, 1000);  
+        //            break;  
+        //        default:  
+        //            break;  
+    }
+}
